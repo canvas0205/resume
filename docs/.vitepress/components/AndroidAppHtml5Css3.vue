69fa0d19804b1616698f8687fe52265807f363d0
t_ogw <canvas0205.dev@gmail.com>
2025-02-11 21:38:36 +0900

    fix

--- docs/.vitepress/components/AndroidAppHtml5Css3.vue
+++ docs/.vitepress/components/AndroidAppHtml5Css3.vue
@@ -20,9 +20,9 @@
 <script setup>
 import ImageGallery from './ImageGallery.vue'; // ImageGallery コンポーネントをインポート
 
-// const PATH = process.env.GITHUB_PAGES === 'true' ? '/resume' : '';
+const PATH = process.env.GITHUB_PAGES === 'true' ? '/resume' : '';
 console.log(import.meta.url)
-const thumbnail = new URL('../../public/images/html5css3/logo.png', import.meta.url).href
+const thumbnail = new URL('/images/html5css3/logo.png', "https://canvas0205.github.io/resume").href
 const preview = [
   new URL('/images/html5css3/top.png', import.meta.url).href,
   new URL('/images/html5css3/quiz_list.png', import.meta.url).href,
